4ccdf86d7dcf3006ce388c625e9383b224a4936d
Megan Alonzo <Megan.Alonzo@iam.local>
2018-11-28 13:08:36 -0600

    setting up level

---
+++ checkpoints/Assets/Scripts/Checkpoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("player"))
+        {
+            Debug.Log("player entered the checkpoint");
+        }
+        
+    }
+}
