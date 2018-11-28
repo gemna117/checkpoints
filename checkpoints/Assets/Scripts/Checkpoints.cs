59c40bdb75ddaeaa3378035c7b75eeb133904b69
Megan Alonzo <Megan.Alonzo@iam.local>
2018-11-28 14:12:11 -0600

    adding to checkpoint script

--- checkpoints/Assets/Scripts/Checkpoints.cs
+++ checkpoints/Assets/Scripts/Checkpoints.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Checkpoints : MonoBehaviour
 {
+    [SerializeField]
+    private float rotationSpeed = 100;
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateRotation()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("player"))
         {
             Debug.Log("player entered the checkpoint");
             PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
+            player.SetCurrentCheckpoint(this);
         }
         
     }
